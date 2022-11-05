391bb6389e077d0ecdcd2b6c0338db6a6bfc428a
Kevin Willian <kevinwillian084@gmail.com>
2022-11-05 18:20:13 -0300

    feat: adiciona novos estilos personalizados e adiciona backgroundImage

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,17 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
         />
       </Head>
-      <body>
+      <body className="bg-gray-900 bg-app bg-no-repeat bg-cover text-white">
         <Main />
         <NextScript />
       </body>
